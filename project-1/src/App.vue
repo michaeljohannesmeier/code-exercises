5b0788a2cf33a2017f0d1f6d4886891ed7480937
Mauricio Erazo <antonierazo13@hotmail.com>
2022-12-27 19:27:44 +0100

    Another try: Delteting the folder using  npm init vue@latest

--- project-1/src/App.vue
+++ project-1/src/App.vue
@@ -1,13 +1,26 @@
 <template>
-  <div>
-    <h1>Hola Mike</h1>
-  </div>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
-<script setup>
+<script>
+import HelloWorld from './components/HelloWorld.vue'
 
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
 </script>
 
-<style lang="scss" scoped>
-
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
--- project-1/src/main.js
+++ project-1/src/main.js
@@ -1,7 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './assets/tailwind.css'
-
-import './assets/main.css'
 
 createApp(App).mount('#app')
