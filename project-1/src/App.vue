6d526d9e5506942951eb96a4dcadc194d631daca
Mauricio Erazo <antonierazo13@hotmail.com>
2022-12-27 20:11:02 +0100

    update text

--- project-1/src/App.vue
+++ project-1/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1 class="text-2xl font-semibold text-lime-900">Hola Mike</h1>
+  <h1 class="text-2xl font-semibold text-lime-900">Hola Mike!</h1>
   <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-6">Calculate</button>
 </template>
 
