0e0a137655f3d0a3cae36b4f43b4cc0744fa905b
Michael Meier <michael_johannes.meier@daimler.com>
2022-12-28 13:48:44 +0100

    new task click listener

--- project-1/src/App.vue
+++ project-1/src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <h1 class="text-2xl font-semibold text-lime-900">Hola Mike!</h1>
+  <h1 class="text-2xl font-semibold text-lime-900">Hola Mau!</h1>
+  <div class="mt-20">Well done, now add a click listener to the calculate button. After pressing the button, the number 1 should be displayed "HERE". After clicking 2 time, 2 should be displayed and so on. Should be an easy task.</div>
   <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-6">Calculate</button>
+  <div class="mt-20">HERE</div>
 </template>
 
 <script>
@@ -20,5 +22,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.mt-20 {
+  margin-top: 20px;
 }
 </style>
