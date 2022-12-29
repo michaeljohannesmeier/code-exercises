c041acd9a3e224f3f0df40ebae0c12ff40c632f8
Mauricio Erazo <antonierazo13@hotmail.com>
2022-12-29 10:46:17 +0100

    Update: times clicked

--- project-1/src/App.vue
+++ project-1/src/App.vue
@@ -1,8 +1,8 @@
 <template>
-   <div class="container flex flex-col items-center">
+  <div class="container flex flex-col items-center">
     <h1 class="text-2xl font-semibold text-lime-900 mt-4">Hola Mike!</h1>
-    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-6" @click="count++">Clicked
-      {{ count }} times</button>
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-6" @click="count++">Calculate</button>
+    <p class="text-2xl font-semibold text-lime-900 mt-2">{{ count }} times clicked</p>
   </div>
 </template>
 
@@ -20,11 +20,3 @@ export default {
 }
 </script>
 
-<style scoped>
-.button {
-  align-content: center;
-}
-.mt-20 {
-  margin-top: 20px;
-}
-</style>
