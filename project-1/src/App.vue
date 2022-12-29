bb50b6e76562fbe796d18a1873c584c63dd87262
Mauricio Erazo <antonierazo13@hotmail.com>
2022-12-29 10:33:03 +0100

    Feat: Adding times clicked on the btn

--- project-1/src/App.vue
+++ project-1/src/App.vue
@@ -1,24 +1,27 @@
 <template>
-  <h1 class="text-2xl font-semibold text-lime-900">Hola Mike!</h1>
-  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-6">Calculate</button>
+  <div class="container flex flex-col items-center">
+    <h1 class="text-2xl font-semibold text-lime-900 mt-4">Hola Mike!</h1>
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-6" @click="count++">Clicked
+      {{ count }} times</button>
+  </div>
 </template>
 
 <script>
-
 export default {
   name: 'App',
   components: {
+  },
+
+  data() {
+    return {
+      count: 0
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.button {
+  align-content: center;
 }
 </style>
