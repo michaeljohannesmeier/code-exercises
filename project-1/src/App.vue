820ec8de16e011616877af719db5c7cdd971dfa0
Mauricio Erazo <antonierazo13@hotmail.com>
2022-12-30 16:21:05 +0100

    update: pulling updates

--- project-1/src/App.vue
+++ project-1/src/App.vue
@@ -1,11 +1,8 @@
 <template>
   <div class="container flex flex-col items-center">
     <div class="flex flex-col items-center">
-      <h1 class="text-2xl font-semibold text-lime-900 mt-4">Hola Mike!</h1>
-      <button
-        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-6"
-        @click="state.count++"
-      >
+      <h1 class="text-2xl font-semibold text-lime-900 mt-4">Hola Amigo!</h1>
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-6" @click="state.count++">
         Calculate
       </button>
       <p class="text-2xl font-semibold text-lime-900 mt-2">
@@ -14,23 +11,13 @@
     </div>
 
     <div class="flex flex-col items-center">
-      <input
-        type="text"
-        placeholder="Search for a joke"
-        v-model="state.searchQuery"
-        @keyup="searchForJokes"
-        class="px-2 py-1 w-3/4 text-black mt-20 bg-slate-300 border-b focus:border-slate-600 focus:outline-none focus:shadow-md"
-      />
+      <input type="text" placeholder="Search for a joke" v-model="state.searchQuery" @keyup="searchForJokes"
+        class="px-2 py-1 w-3/4 text-black mt-20 bg-slate-300 border-b focus:border-slate-600 focus:outline-none focus:shadow-md" />
       <!-- <p v-if="!serverError && jokesSearchResult.lenth === 0">No result match, try again</p> -->
-      <div
-        class="joke-container mt-5 bg-fuchsia-200 rounded p-10 mx-10 text-center shadow-md"
-      >
+      <div class="joke-container mt-5 bg-fuchsia-200 rounded p-10 mx-10 text-center shadow-md">
         <h2 class="text-3xl border-b-2">The great Chuck!</h2>
         <h3 class="text-2xl mt-10 text-black">{{ state.joke }}</h3>
-        <button
-          class="bg-rose-400 hover:bg-rose-500 text-white font-bold p-2 rounded-md mt-14"
-          @click="getRandomJoke"
-        >
+        <button class="bg-rose-400 hover:bg-rose-500 text-white font-bold p-2 rounded-md mt-14" @click="getRandomJoke">
           Give me a random Joke
         </button>
       </div>
