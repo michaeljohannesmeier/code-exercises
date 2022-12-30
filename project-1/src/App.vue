136598b4689a431a2a76ae1ddaf6d86aa42d247a
Michael Meier <michael_johannes.meier@daimler.com>
2022-12-30 14:55:35 +0100

    updates

--- project-1/src/App.vue
+++ project-1/src/App.vue
@@ -2,41 +2,60 @@
   <div class="container flex flex-col items-center">
     <div class="flex flex-col items-center">
       <h1 class="text-2xl font-semibold text-lime-900 mt-4">Hola Mike!</h1>
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-6"
-        @click="state.count++">Calculate</button>
-      <p class="text-2xl font-semibold text-lime-900 mt-2">{{ state.count }} times clicked</p>
+      <button
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-6"
+        @click="state.count++"
+      >
+        Calculate
+      </button>
+      <p class="text-2xl font-semibold text-lime-900 mt-2">
+        {{ state.count }} times clicked
+      </p>
     </div>
 
     <div class="flex flex-col items-center">
-      <input type="text" placeholder="Search for a joke" v-mode="searchQuery" @input="searchForJokes"
-        class="px-2 py-1 w-3/4 text-black mt-20 bg-slate-300 border-b focus:border-slate-600 focus:outline-none focus:shadow-md">
+      <input
+        type="text"
+        placeholder="Search for a joke"
+        v-model="state.searchQuery"
+        @keyup="searchForJokes"
+        class="px-2 py-1 w-3/4 text-black mt-20 bg-slate-300 border-b focus:border-slate-600 focus:outline-none focus:shadow-md"
+      />
       <!-- <p v-if="!serverError && jokesSearchResult.lenth === 0">No result match, try again</p> -->
-      <div class="joke-container mt-5 bg-fuchsia-200 rounded p-10 mx-10 text-center shadow-md">
+      <div
+        class="joke-container mt-5 bg-fuchsia-200 rounded p-10 mx-10 text-center shadow-md"
+      >
         <h2 class="text-3xl border-b-2">The great Chuck!</h2>
         <h3 class="text-2xl mt-10 text-black">{{ state.joke }}</h3>
-        <button class="bg-rose-400 hover:bg-rose-500 text-white font-bold p-2 rounded-md mt-14" @click="getRandomJoke">
-          Give me a random Joke</button>
+        <button
+          class="bg-rose-400 hover:bg-rose-500 text-white font-bold p-2 rounded-md mt-14"
+          @click="getRandomJoke"
+        >
+          Give me a random Joke
+        </button>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { reactive } from 'vue'
-import axios from 'axios'
+import { reactive } from 'vue';
+import axios from 'axios';
 
-
-const state = reactive({ count: 0, joke: null, randomNumber: null })
-const jokeAPI = 'https://api.chucknorris.io/jokes/random'
-
-const searchQuery = reactive("")
-const queryTimeout = reactive(null)
-const jokesSearchResult = reactive(null)
+const state = reactive({
+  count: 0,
+  joke: null,
+  randomNumber: null,
+  searchQuery: '',
+  queryTimeout: null,
+  jokesSearchResult: null,
+});
+const jokeAPI = 'https://api.chucknorris.io/jokes/random';
 
 const getRandomJoke = async () => {
-  const result = await axios(jokeAPI)
-  state.joke = result.data.value
-}
+  const result = await axios(jokeAPI);
+  state.joke = result.data.value;
+};
 
 // Not using Async and await
 // --------------------------
@@ -51,17 +70,16 @@ const getRandomJoke = async () => {
 // };
 
 const searchForJokes = async () => {
-  clearTimeout(queryTimeout.value)
-  queryTimeout.value = setTimeout(async () => {
-    if (searchQuery.value !== "") {
-      const result = await axios(`https://api.chucknorris.io/jokes/search?query=${searchQuery.value}`)
-      jokesSearchResult.value = result.data.value
-      console.log(jokesSearchResult.value)
+  console.log(state.searchQuery);
+  clearTimeout(state.queryTimeout);
+  state.queryTimeout = setTimeout(async () => {
+    if (state.searchQuery !== '') {
+      const result = await axios(
+        `https://api.chucknorris.io/jokes/search?query=${state.searchQuery}`
+      );
+      state.jokesSearchResult = result.data;
+      console.log(state.jokesSearchResult);
     }
-
-    return
-
-  }, 600)
-}
-
+  }, 600);
+};
 </script>
