598b0ff4dfc00e8222a5519c4e98835484b351d7
Mauricio Erazo <antonierazo13@hotmail.com>
2022-12-30 14:07:21 +0100

    issue 5: I don't quite understand what to do with the loop to call the first 3 jokes.
    This is what I have done so far.

--- project-1/src/App.vue
+++ project-1/src/App.vue
@@ -1,16 +1,22 @@
 <template>
   <div class="container flex flex-col items-center">
-    <h1 class="text-2xl font-semibold text-lime-900 mt-4">Hola Mike!</h1>
-    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-6"
-      @click="state.count++">Calculate</button>
-    <p class="text-2xl font-semibold text-lime-900 mt-2">{{ state.count }} times clicked</p>
+    <div class="flex flex-col items-center">
+      <h1 class="text-2xl font-semibold text-lime-900 mt-4">Hola Mike!</h1>
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-6"
+        @click="state.count++">Calculate</button>
+      <p class="text-2xl font-semibold text-lime-900 mt-2">{{ state.count }} times clicked</p>
+    </div>
 
-    <div class="joke-container mt-20 bg-fuchsia-200 rounded p-10 mx-10 text-center shadow-md">
-      <h2 class="text-3xl border-b-2">The great Chuck!</h2>
-      <h3 class="text-2xl mt-10 text-black">{{ state.joke }}</h3>
-      <button class="bg-rose-400 hover:bg-rose-500 text-white font-bold p-2 rounded-md mt-14"
-        @click="getRandomJoke">Give a
-        Give me a random Joke</button>
+    <div class="flex flex-col items-center">
+      <input type="text" placeholder="Search for a joke" v-mode="searchQuery" @input="searchForJokes"
+        class="px-2 py-1 w-3/4 text-black mt-20 bg-slate-300 border-b focus:border-slate-600 focus:outline-none focus:shadow-md">
+      <!-- <p v-if="!serverError && jokesSearchResult.lenth === 0">No result match, try again</p> -->
+      <div class="joke-container mt-5 bg-fuchsia-200 rounded p-10 mx-10 text-center shadow-md">
+        <h2 class="text-3xl border-b-2">The great Chuck!</h2>
+        <h3 class="text-2xl mt-10 text-black">{{ state.joke }}</h3>
+        <button class="bg-rose-400 hover:bg-rose-500 text-white font-bold p-2 rounded-md mt-14" @click="getRandomJoke">
+          Give me a random Joke</button>
+      </div>
     </div>
   </div>
 </template>
@@ -19,8 +25,13 @@
 import { reactive } from 'vue'
 import axios from 'axios'
 
+
 const state = reactive({ count: 0, joke: null, randomNumber: null })
 const jokeAPI = 'https://api.chucknorris.io/jokes/random'
+
+const searchQuery = reactive("")
+const queryTimeout = reactive(null)
+const jokesSearchResult = reactive(null)
 
 const getRandomJoke = async () => {
   const result = await axios(jokeAPI)
@@ -30,7 +41,7 @@ const getRandomJoke = async () => {
 // Not using Async and await
 // --------------------------
 // const getRandomJoke = () => {
-//   /* 
+//   /*
 //     - this is the short versino of getRandomJoke2
 //     - axios makes the request, and then you ll get the res (response), but see getRandomJoke2 to better understand
 //   */
@@ -38,4 +49,19 @@ const getRandomJoke = async () => {
 //     state.joke = res.data.value;
 //   });
 // };
+
+const searchForJokes = async () => {
+  clearTimeout(queryTimeout.value)
+  queryTimeout.value = setTimeout(async () => {
+    if (searchQuery.value !== "") {
+      const result = await axios(`https://api.chucknorris.io/jokes/search?query=${searchQuery.value}`)
+      jokesSearchResult.value = result.data.value
+      console.log(jokesSearchResult.value)
+    }
+
+    return
+
+  }, 600)
+}
+
 </script>
