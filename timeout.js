a91dc355641403d060c640c500db67b828d1fd1b
Mauricio Erazo <antonierazo13@hotmail.com>
2023-01-16 21:06:30 +0100

    update: less lines of code

--- timeout.js
+++ timeout.js
@@ -133,12 +133,8 @@ const sortByName = people.sort((a, b) => {
   let nameA = a.name.toUpperCase()
   let nameB = b.name.toUpperCase()
 
-  if (nameA < nameB) {
-    return -1
-  }
-  if (nameA > nameB) {
-    return 1
-  }
+  if (nameA < nameB) return -1
+  if (nameA > nameB) return 1
   return 0
 })
 console.log(sortByName)
